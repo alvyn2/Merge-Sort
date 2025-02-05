d0e818f14477e2af9a2d108667ca5b39f849fc88
alvyn2 <87878122+alvyn2@users.noreply.github.com>
2025-02-05 15:15:07 +0000

    first merge works

--- workspace/Main.java
+++ workspace/Main.java
@@ -15,64 +15,68 @@ public static void sort(int[]arr){
     int[] arr2 = new int[arr.length];
     int chunkSize=2;
     int l=0;
-    int r=0;
+    int r=l+chunkSize/2;
     int copyi=0;//copyindex
     int[] c=arr;//c for current
-        int[] copy=arr2;
+    int[] copy=arr2;
+    int start=l;
     System.out.println("original array");
     for(int n:arr){
         System.out.println(n);
     }
-    while(chunkSize<=arr.length){
-        r=l+chunkSize;
-        //chunkSize*=2;
-        while(l<arr.length-1){
-        
-        for(int i=l;i<r;i++){
-            if(c[i]<c[i+1]){
+    //imagine each individual value is its own array already
+    while(chunkSize<=arr.length-1){
+      
+        start=l;
+        while(l<=arr.length){
+        while(l<start+chunkSize/2 || r<start+chunkSize && r<=arr.length){
+ 
+        for(int i=l;i<arr.length-1;i+=chunkSize){
+            if(c[i]<c[i+chunkSize/2]){
                 copy[copyi]=c[i];
+                copy[copyi+1]=c[i+1];
             }else{
                 copy[copyi]=c[i+1];
-            }
-            System.out.println("copied "+copy[copyi]);
-            copyi++;
+                copy[copyi+1]=c[i];
+        }
+            System.out.println("copied "+copy[copyi]+" and "+copy[copyi+1]);
+            copyi+=chunkSize;
         }
         l=r+1;
         r=l+chunkSize/2;
         copyi=l;
         }
-        //System.out.println("loop 1 run");
+        System.out.println(" inner loop ended");
+        l=r+1;
+        r=l+chunkSize/2;
+        //copyi=l;
+        for(int n:arr){
+            System.out.println(n);
+            }
+            if(c==arr){
+                c=arr2;
+                copy=arr;
+            }else{
+                c=arr;
+                copy=arr2;
+            }
+    }
+        
     System.out.println("arr one chunk"+chunkSize);
-    for(int n:arr){
-    System.out.println(n);
-    }
-    if(c==arr){
-        c=arr2;
-        copy=arr;
-    }else{
-        c=arr;
-        copy=arr2;
-    }
+    
     System.out.println("arr2");
     for(int n:c){
         System.out.println(n);
     }
 
-        if(c==arr){
-            c=arr2;
-            copy=arr;
-        }else{
-            c=arr;
-            copy=arr2;
-        }
 
     chunkSize*=2;
     }
-    //imagine each individual value is its own array already
-    //old code
+    
+    //old code using for loops
    
-    /* 
-    for(int i=0;i<arr.length;i+=2){
+     /* 
+    for(int i=0;i<arr.length-1;i++){
     //copies and sorts into sorted sections of 2 elements each
     if(arr[i]<arr[i+1]){
         
@@ -88,7 +92,7 @@ public static void sort(int[]arr){
     for(int n:arr2){
     System.out.println(n);
     }
-    
+     
     for(int i=0;i<arr.length;i+=4){
         //copies and sorts into sorted sections of 4 elements each
         for(int j=i;j<(arr.length)/2;j+=2){
